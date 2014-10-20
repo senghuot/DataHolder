791bc9ae093e95824191ba5909b6cc15b9b90543
senghuot <senghuotlim@gmail.com>
2014-10-20 00:27:14 -0700

    Adding README and Splay implementation

--- src/SplayTree.java
+++ src/SplayTree.java
@@ -22,27 +22,63 @@ public class SplayTree {
 	}
    
 	/**
+	 * 
+	 * @param key
+	 */
+	public void splay(int key) {
+		root = splay(root, key);
+	}
+	
+	/**
+	 * 
+	 * @param root
+	 * @param key
+	 * @return
+	 */
+	private SplayNode splay(SplayNode root, int key) {
+		if(root == null)
+			return null;
+		
+		if()
+		
+		return null;
+	}
+	
+	/**
 	 * add a new node to the tree.
 	 * @param int value stores as a node.
 	 */
 	public void insert(int value) {
-		root = insert(root, value);
+		// the tree is initially empty.
+		if(root == null)
+			root = new SplayNode(value);
+		
+		// splaying will bring the key to root to only then decide 
+		// weather duplicate occur else we would simply insert a new key.
+		root = splay(root, value);
+		if(value > root.getKey()) {
+			SplayNode tmp = new SplayNode(value);
+			tmp.left = root;
+			tmp.right = root.right;
+			root.right = null;
+			root = tmp;
+		} else if(value < root.getKey()) {
+			SplayNode tmp = new SplayNode(value);
+			tmp.right = root;
+			tmp.left = root.left;
+			root.left = null;
+			root = tmp;
+		}
 	}
 	
 	/**
-	 * helper method to help adding new node to the tree.
-	 * @param curr as a root of the tree.
-	 * @param value as a node to be inserted.
-	 * @return a node representation of the new tree.
+	 * Print out the current state of the tree using preorder tra
 	 */
-	private SplayNode insert(SplayNode root, int value) {
-		if(root == null)
-			return new SplayNode(value);
+	public void display() {
+		display(root);
+	}
+	
+	private void display(SplayNode root) {
 		
-		if(value <= root.getKey())
-			root.left = insert(root.left, value);
-		else
-			root.right = insert(root.right, value);
-		return root;
 	}
 }
