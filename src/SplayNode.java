02f3430a4a1ddffd091f5b802c967e96391110d1
senghuot <senghuotlim@gmail.com>
2014-10-19 23:09:49 -0700

    Adding add method

--- src/SplayNode.java
+++ src/SplayNode.java
@@ -1,8 +1,29 @@
+import hw1.Node;
+
 /**
- * 
+ * This string
  * @author senghuot
  */
 
 public class SplayNode {
-
+	private int key;
+	SplayNode left;
+	SplayNode right;
+	
+	/**
+	 * @param key to be stored for the newly constructed node.
+	 * @effect construct a new node base on given key.
+	 */
+	public SplayNode(int key) {
+		this.key = key;
+		left = null;
+		right = null;
+	}
+	
+	/**
+	 * @return key, int of the current node.
+	 */
+	public int getKey() {
+		return key;
+	}
 }
--- src/SplayTree.java
+++ src/SplayTree.java
@@ -5,4 +5,44 @@
 
 public class SplayTree {
 
+	private SplayNode root;
+	
+	/**
+	 * @effect construct a new Tree with root sets to null
+	 */
+	public SplayTree() {
+		this(null);
+	}
+	/**
+	* @param root as a root of the tree.
+	* @effect construct a new Tree with user given root.
+	*/
+	public SplayTree(SplayNode root) {
+		this.root = root;
+	}
+   
+	/**
+	 * add a new node to the tree.
+	 * @param int value stores as a node.
+	 */
+	public void add(int value) {
+		root = add(root, value);
+	}
+	
+	/**
+	 * helper method to help adding new node to the tree.
+	 * @param curr as a root of the tree.
+	 * @param value as a node to be inserted.
+	 * @return a node representation of the new tree.
+	 */
+	private SplayNode add(SplayNode root, int value) {
+		if(root == null)
+			return new SplayNode(value);
+		
+		if(value <= root.getKey())
+			root.left = add(root.left, value);
+		else
+			root.right = add(root.right, value);
+		return root;
+	}
 }
