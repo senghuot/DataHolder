c4f11854ff488d59085ffe0de175bea7fdf822ac
unknown <senghuotlim@gmail.com>
2014-11-09 00:56:59 -0800

    Version 1 off by one error

--- src/PopulationQuery.java
+++ src/PopulationQuery.java
@@ -66,18 +66,71 @@ public class PopulationQuery {
 		test[3] = "-v1";
 		args = test;
 		
-		CensusData data = parse("CenPop2010.txt");
-		
 		// shut down the problem if user doesn't provide at least 4 arguments
 		if (args.length < 4)
 			System.exit(0);
 		
-		// performing our user inputs
-		Scanner scanner = new Scanner(System.in);
-		do {
+		// might have to re-design the class. possibly having a superclass then
+		// four different version of class
+		if (args[3].equals("-v1")) {
+			CensusData census = parse(args[0]);
+			Scanner console = new Scanner(System.in);
+			
+			// break and read the input from user.
+			int[][] pop = getCensusGrid(100, 500, census);
 			System.out.println("Please give west, south, east, north coordinates of your query");
+			String[] input = console.nextLine().split(" ");
+			while (input.length == 4) {
+				int minX = Integer.parseInt(input[0]) - 1;
+				int maxX = Integer.parseInt(input[2]);
+				int minY = Integer.parseInt(input[1]) - 1;
+				int maxY = Integer.parseInt(input[3]);
+				
+				int total = 0;
+				for (int i=minX; i<maxX; i++) {
+					for (int j=minY; j<maxY; j++)
+						total += pop[i][j];
+				}
+				System.out.println(total);
+				
+				System.out.println("Please give west, south, east, north coordinates of your query");
+				input = console.nextLine().split(" ");
+			}
+		}
+	}
+	
+	/**
+	 * create a two dimensional array of int for storing population
+	 */
+	public static int[][] getCensusGrid(int x, int y, CensusData census) {
+		int[][] grid = new int[x][y];
+		
+		// find the Min/Max for longitude and latitude 
+		if (census.data_size > 0) {
+			float minX = census.data[0].latitude; 
+			float maxX = census.data[0].latitude;
+			float minY = census.data[0].longitude;
+			float maxY = census.data[0].longitude;
+
+			for (int i = 1; i < census.data_size; i++) {
+				minX = Math.min(minX, census.data[i].latitude);
+				maxX = Math.max(maxX, census.data[i].latitude);
+				minY = Math.min(minY, census.data[i].longitude);
+				maxY = Math.max(maxY, census.data[i].longitude);
+			}
 			
-			
-		} while(false);
+			// inserting the population into the population grid
+			float widthX = (maxX - minX) / x;
+			float widthY = (maxY - minY) / y;
+			for (int i = 0; i < census.data_size; i++) {
+				int gridX = (int) Math.ceil((census.data[i].latitude - minX) / widthX);
+				int gridY = (int) Math.ceil((census.data[i].longitude - minY) / widthY);
+				gridX = Math.min(gridX, x - 1);
+				gridY = Math.min(gridY, y - 1);
+				grid[gridX][gridY] += census.data[i].population;
+			}
+		}
+
+		return grid;
 	}
 }
